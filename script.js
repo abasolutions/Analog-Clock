6f48615a53aeb917bdc6410b3e06aa87b5cf9e14
abasolutions <135368841+abasolutions@users.noreply.github.com>
2023-06-28 17:33:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+setInterval(() => {
+    date = new Date();
+    hour = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+
+    hh = document.querySelector(".hour-hand");
+    mh = document.querySelector(".minute-hand");
+    sh = document.querySelector(".second-hand");
+
+    hh_rot = (30 * hour) + 90;
+    mh_rot = (6 * min) + 90;
+    sh_rot = (6 * sec) + 90;
+
+    console.log(sec);
+
+    sh.style.transform = `rotate(${sh_rot}deg)`;
+    mh.style.transform = `rotate(${mh_rot}deg)`;
+    hh.style.transform = `rotate(${hh_rot}deg)`;
+}, 1000);
